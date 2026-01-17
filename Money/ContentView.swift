5f50be2979c09c0b531d1fa8a81a46fc633f63a0
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-01-17 17:39:36 +0800

    Initial Commit

---
+++ Money/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Money
+//
+//  Created by Adon Omeri on 17/1/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Money/MoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoneyApp.swift
+//  Money
+//
+//  Created by Adon Omeri on 17/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct MoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
