9fb436acab065824f759d510bc38beac0e0cb6e8
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-01-21 20:05:46 +0800

    added watch target

---
+++ moneyWatch Watch App/moneyWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  moneyWatchApp.swift
+//  moneyWatch Watch App
+//
+//  Created by Adon Omeri on 21/1/2026.
+//
+
+import SwiftUI
+
+@main
+struct moneyWatch_Watch_AppApp: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
