6b28f2607fd5851f7e24a9bb76aecd31faf7b66d
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-02-12 23:12:00 +0800

    refine

--- Shared/Backend/Defaults.swift
+++ Shared/Backend/Defaults.swift
@@ -12,7 +12,11 @@ extension Defaults.Keys {
 	static let userToken = Key<String?>("userToken", default: nil)
 	static let userEmail = Key<String?>("userEmail", default: nil)
 	static let userFirstName = Key<String?>("userFirstName", default: nil)
-	static let transactions = Key<[Transaction]>("transactions", default: [], suite: .init(suiteName: "group.omeriadon.money")!)
+	#if DEBUG
+		static let transactions = Key<[Transaction]>("transactions", default: [], suite: .init(suiteName: "group.omeriadon.money")!)
+	#else
+		static let transactions = Key<[Transaction]>("transactions", default: [], suite: .init(suiteName: "group.omeriadon-hackclub-release.money")!)
+	#endif
 
 	static let useNewGradient = Key<Bool>("useNewGradient", default: true)
 	static let showAnalyseTab = Key<Bool>("showAnalyseTab", default: true)
