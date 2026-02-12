19d830d27eed5eb46d64c7c9d4c46234ff57da98
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-02-12 22:31:35 +0800

    added others

--- Money Widgets/Money_Widgets.swift
+++ Money Widgets/Money_Widgets.swift
@@ -11,7 +11,7 @@ import WidgetKit
 
 struct Provider: TimelineProvider {
 	func placeholder(in _: Context) -> SimpleEntry {
-		SimpleEntry(date: Date(), total: 0.00)
+		SimpleEntry(date: Date(), total: 100.00)
 	}
 
 	func getSnapshot(in _: Context, completion: @escaping (SimpleEntry) -> Void) {
@@ -36,13 +36,40 @@ struct SimpleEntry: TimelineEntry {
 struct Money_WidgetsEntryView: View {
 	var entry: Provider.Entry
 
+	@Environment(\.widgetFamily) var widgetFamily
+
 	var body: some View {
-		Text(entry.total, format: .currency(code: "AUD"))
-			.foregroundStyle(.white)
-			.font(.system(size: 1000))
-			.monospaced()
-			.lineLimit(1)
-			.minimumScaleFactor(0.01)
+		switch widgetFamily {
+			case .systemLarge, .systemMedium, .systemSmall:
+				Text(entry.total, format: .currency(code: "AUD"))
+					.foregroundStyle(.white)
+					.font(.system(size: 1000))
+					.monospaced()
+					.lineLimit(1)
+					.minimumScaleFactor(0.01)
+			case .accessoryCorner:
+				Text(Image("Logo"))
+					.font(.system(size: 10))
+					.widgetCurvesContent()
+					.widgetLabel {
+						Text(entry.total, format: .currency(code: "AUD"))
+							.monospaced()
+					}
+			case .accessoryInline:
+				Text(entry.total, format: .currency(code: "AUD"))
+			// should be accessoryRectangular
+			// accessoryCircular not supported
+			default:
+				VStack(alignment: .trailing) {
+					Text("Money")
+						.font(.body)
+
+					Text(entry.total, format: .currency(code: "AUD"))
+						.font(.system(size: 300))
+						.lineLimit(1)
+						.minimumScaleFactor(0.001)
+				}
+		}
 	}
 }
 
@@ -56,10 +83,41 @@ struct Money_Widgets: Widget {
 		}
 		.configurationDisplayName("Total")
 		.description("Sum of all transactions.")
+		.supportedFamilies([.systemSmall, .systemMedium, .systemLarge, .systemExtraLarge, .accessoryInline, .accessoryRectangular])
 	}
 }
 
 #Preview(as: .systemSmall) {
+	Money_Widgets()
+} timeline: {
+	SimpleEntry(date: .now, total: 1234.56)
+}
+
+#Preview(as: .systemMedium) {
+	Money_Widgets()
+} timeline: {
+	SimpleEntry(date: .now, total: 1234.56)
+}
+
+#Preview(as: .systemLarge) {
+	Money_Widgets()
+} timeline: {
+	SimpleEntry(date: .now, total: 1234.56)
+}
+
+#Preview(as: .systemExtraLarge) {
+	Money_Widgets()
+} timeline: {
+	SimpleEntry(date: .now, total: 1234.56)
+}
+
+#Preview(as: .accessoryInline) {
+	Money_Widgets()
+} timeline: {
+	SimpleEntry(date: .now, total: 1234.56)
+}
+
+#Preview(as: .accessoryRectangular) {
 	Money_Widgets()
 } timeline: {
 	SimpleEntry(date: .now, total: 1234.56)
