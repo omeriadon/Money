eff7711eef4d9c223e4cc5d20188026aba0450f9
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-02-28 20:35:50 +0800

    add typed whats-new seen state to defaults

--- Shared/Backend/Defaults.swift
+++ Shared/Backend/Defaults.swift
@@ -8,10 +8,36 @@
 import Defaults
 import Foundation
 
+struct WhatsNewRelease: Codable, Hashable, Defaults.Serializable, Comparable {
+	let version: String
+	let build: Int
+
+	static func < (lhs: WhatsNewRelease, rhs: WhatsNewRelease) -> Bool {
+		let lhsParts = lhs.version.split(separator: ".").map { Int($0) ?? 0 }
+		let rhsParts = rhs.version.split(separator: ".").map { Int($0) ?? 0 }
+		let maxCount = max(lhsParts.count, rhsParts.count)
+
+		for index in 0 ..< maxCount {
+			let l = index < lhsParts.count ? lhsParts[index] : 0
+			let r = index < rhsParts.count ? rhsParts[index] : 0
+			if l != r { return l < r }
+		}
+
+		return lhs.build < rhs.build
+	}
+}
+
+enum WhatsNewSeenState: Codable, Defaults.Serializable, Equatable {
+	case unseen
+	case release(WhatsNewRelease)
+	case resetRequested
+}
+
 extension Defaults.Keys {
 	static let userEmail = Key<String?>("userEmail", default: nil)
 	static let userFirstName = Key<String?>("userFirstName", default: nil)
 	static let hasSeenIntroSplash = Key<Bool>("hasSeenIntroSplash", default: false)
+	static let whatsNewSeenState = Key<WhatsNewSeenState>("whatsNewSeenState", default: .unseen)
 
 	private static let appGroupSuite = UserDefaults(suiteName: AppConfig.appGroupSuiteName)!
 
