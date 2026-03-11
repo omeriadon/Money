e06dbd6f8587427bb064914cf9dccd6d99f4ef4a
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-03-11 16:40:47 +0800

    whatsnewsstuff

--- Money/Views/Whats New/Data/1_0__10.swift
+++ Money/Views/Whats New/Data/1_0__10.swift
@@ -8,6 +8,11 @@
 extension WhatsNewReleaseCatalog {
 	static let r1_0__10: [WhatsNewItem] = [
 		WhatsNewItem(
+			title: "Goals",
+			description: "Make goals and track goals, set with widgets and complications too. Structure and design will change in the future.",
+			symbolName: "target"
+		),
+		WhatsNewItem(
 			title: "Version Information",
 			description: "Check version or build information in settings.",
 			symbolName: "gear"
